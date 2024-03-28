ba4f498ab2207a13288a12d485a4eacee08b86fb
Loïse Fenoll <admin@fingertips.fr>
2024-03-28 12:08:21 +0100

    use expo router

--- firebase/auth_github_signin_popup.js
+++ firebase/auth_github_signin_popup.js
@@ -6,7 +6,6 @@ export const signinWithGithub = () => {
     console.log("signinWithGithub")
     signInWithPopup(auth, provider)
         .then((result) => {
-            console.log(result)
             // This gives you a GitHub Access Token. You can use it to access the GitHub API.
             const credential = GithubAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,5 @@ const { getDefaultConfig } = require('@expo/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
-
+defaultConfig.resolver.extraNodeModules["stream/web"] = require.resolve("readable-stream")
 module.exports = defaultConfig;
